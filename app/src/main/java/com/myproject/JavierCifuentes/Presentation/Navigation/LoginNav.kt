5c4118ca2f1ba26a94d393c25d85fd9706ce9d65
JaviCC19 <cif23079@uvg.edu.gt>
2025-02-09 15:07:10 -0600

    Commit Inicial, estuctura y login

---
+++ app/src/main/java/com/myproject/JavierCifuentes/Presentation/Navigation/LoginNav.kt
@@ -0,0 +1,24 @@
+package com.myproject.JavierCifuentes.Presentation.Navigation
+
+import androidx.navigation.compose.composable
+import kotlinx.serialization.Serializable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraphBuilder
+import com.myproject.JavierCifuentes.Presentation.Screens.LoginRoute
+
+
+@Serializable
+data object LoginDestination
+
+fun NavGraphBuilder.loginScreen(
+    onLoginClick: () -> Unit
+){
+    composable<LoginDestination>{
+        LoginRoute(
+            onLoginClick = onLoginClick,
+            modifier = Modifier.fillMaxWidth()
+
+        )
+    }
+}
---
+++ app/src/main/java/com/myproject/JavierCifuentes/Presentation/Screens/LoginScreen.kt
@@ -0,0 +1,121 @@
+package com.myproject.JavierCifuentes.Presentation.Screens
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.myproject.JavierCifuentes.Data.DataStore.DataStoreManager
+import com.myproject.JavierCifuentes.R
+import kotlinx.coroutines.launch
+
+@Composable
+fun LoginRoute(onLoginClick: () -> Unit,
+               modifier: Modifier = Modifier
+){
+    LoginScreen(onLoginClick = onLoginClick, modifier = modifier)
+}
+
+@Composable
+fun LoginScreen(onLoginClick: () -> Unit,
+                modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current
+    val dataStoreManager = remember { DataStoreManager(context) }
+    val scope = rememberCoroutineScope()
+
+    var userName by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+
+        Image(
+            painter = painterResource(R.drawable.recefacillogo),
+            contentDescription = "Logo de recefacil",
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        OutlinedTextField(
+            value = userName,
+            onValueChange = { userName = it },
+            label = { Text("Ingresa el usuario") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Ingresa la contraseña") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                scope.launch {
+                    if (userName.isNotEmpty()) {
+                        dataStoreManager.saveUserName(userName)
+                        onLoginClick()
+                    } else {
+                        Toast.makeText(context, "Debe ingresar un usuario", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 30.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            )
+        ) {
+            Text(text = "Entrar")
+        }
+
+        Spacer(modifier = Modifier.height(300.dp))
+
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewLoginScreen() {
+    LoginScreen(onLoginClick = {})
+}
